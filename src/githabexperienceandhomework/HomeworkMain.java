4ec775297b608bad6d3f7807582da93dcc393b4a
Anna Gishyan <anna.gishyang@gmail.com>
2022-03-26 15:01:05 +0400

    changed

--- src/githabexperienceandhomework/HomeworkMain.java
+++ src/githabexperienceandhomework/HomeworkMain.java
@@ -4,6 +4,7 @@ public class HomeworkMain {
     public static void main(String[] args) {
         if (5<6) {
             System.out.println("Greatings");
+            System.out.println("hi");
         }
     }
 }
