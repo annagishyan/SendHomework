ac4e58047f39cb5ab33a8c45a1718b1c8ffabe5c
Anna Gishyan <anna.gishyang@gmail.com>
2022-03-26 14:55:06 +0400

    new file

---
+++ src/githabexperienceandhomework/HomeworkMain.java
@@ -0,0 +1,9 @@
+package githabexperienceandhomework;
+
+public class HomeworkMain {
+    public static void main(String[] args) {
+        if (5<6) {
+            System.out.println("Greatings");
+        }
+    }
+}
